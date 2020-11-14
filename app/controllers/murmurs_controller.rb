f1b7f81db044bd9aa86020472e5943025ec9d50f
masao takayama <m.takayama0929@gmail.com>
2020-11-14 11:16:54 +0000

    edit,updateアクションの追加

--- app/controllers/murmurs_controller.rb
+++ app/controllers/murmurs_controller.rb
@@ -2,19 +2,36 @@ class MurmursController < ApplicationController
   def index
     @murmurs = Murmur.all
   end
+
   def new
     @murmur = Murmur.new
   end
+
   def create
     @murmur = Murmur.create(murmur_params)
     if @murmur.save
-      redirect_to new_murmur_path, notice: "つぶやきを投稿しました！"
+      redirect_to murmurs_path, notice: "つぶやきを投稿しました！"
     else
       render :new
     end
   end
+
+  def edit
+    @murmur = Murmur.find(params[:id])
+  end
+
+  def update
+    @murmur = Murmur.find(params[:id])
+    if @murmur.update(murmur_params)
+      redirect_to murmurs_path, notice: "つぶやきを編集しました！"
+    else
+      render :edit
+    end
+  end
+
   private
   def murmur_params
     params.require(:murmur).permit(:content)
   end
+
 end
